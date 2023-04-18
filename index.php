7e9f6748c73f02e02bf466e16b8974ed44cda4ab
theKlysm <130249836+theKlysm@users.noreply.github.com>
2023-04-17 23:20:45 -0300

    Add header in the index file in addition to href link of fonts

--- index.php
+++ index.php
@@ -3,9 +3,36 @@
     <head>
         <title>Facebook - Login</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <<link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+ 
     </head>
 
     <body>
+        <header>
+            <div class="center">
+                <div class="logo">
+                    <h2>Facebook</h2>
+                </div><!-- Logo -->
 
+                <form method="post" class="form-login">
+                    <div class="form-element">
+                        <p>E-mail ou telefone:</p>
+                        <input type="email"/>
+                    </div><!-- Font element email -->
+
+                    <div class="form-element">
+                        <p>Senha:</p>
+                        <input type="password"/>
+                    </div><!-- Font element password -->
+
+                    <div class="form-element">
+                        <p>E-mail ou telefone:</p>
+                        <input type="submit" name="acao" value="Enviar"/>
+                    </div><!-- Font element submit -->
+                </form><!-- form login -->
+            </div><!-- Center -->
+        </header>
     </body>
 </html>
